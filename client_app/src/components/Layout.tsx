87da2c1cbf6c5d9bf0c5e7a31e6d514115f39e17
komarbv211 <andrijkoslak@gmail.com>
2025-03-19 00:36:19 +0200

    Added client_app

---
+++ client_app/src/components/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
+
+const Layout: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="flex-1 p-4">
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ client_app/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold">Головна сторінка</h1>
+      <p className="text-lg mt-2">Ласкаво просимо до нашого додатку!</p>
+    </div>
+  );
+};
+
+export default Home;
