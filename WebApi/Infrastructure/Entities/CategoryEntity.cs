f977eb34405a62a50db92230afaa47fb97ca667c
novakvova <novakvova@gmail>
2025-03-19 18:53:46 +0200

    Begin entity. Start migration.

---
+++ WebApi/Infrastructure/Entities/CategoryEntity.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Entities;
+
+[Table("tblCategories")]
+public class CategoryEntity : BaseEntity<long>
+{
+    [Required, StringLength(200)]
+    public string Name { get; set; } = string.Empty;
+    [Required, StringLength(255)]
+    public string UrlSlug { get; set; } = string.Empty;
+
+    public int Priority { get; set; }
+    [StringLength(255)]
+    public string Image { get; set; } = string.Empty;
+
+    [StringLength(4000)]
+    public string Description { get; set; } = string.Empty;
+
+    [ForeignKey("Parent")]
+    public long? ParentId { get; set; }
+    public virtual CategoryEntity Parent { get; set; } = null!;
+
+    public virtual ICollection<CategoryEntity> Children { get; set; } = [];
+}
