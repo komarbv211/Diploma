cf2d79ccc4695d84a153af987320b7b3f0fdd23b
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-03-21 00:04:44 +0200

    CREATE CLASS SEEDER & CREATE file Category json

---
+++ WebApi/Infrastructure/Extensions/DbSeeder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Infrastructure.Extensions
+{
+    public class DbSeeder
+    {
+         //Category seeder
+            var categoryRepo = scope.ServiceProvider.GetService<IRepository<Category>>();
+            if (categoryRepo is not null && !await categoryRepo.AnyAsync())
+            {
+                Console.WriteLine("Start categories seeder");
+                string categoryJsonDataFile = Path.Combine(Environment.CurrentDirectory, app.Configuration["SeederJsonDir"]!,"Categories.json");
+                if (File.Exists(categoryJsonDataFile))
+                {
+                    var filtersJson = File.ReadAllText(categoryJsonDataFile, Encoding.UTF8);
+                    try
+                    {
+                        var categoryModels = JsonConvert.DeserializeObject<IEnumerable<SeederCategoryModel>>(filtersJson)
+                            ?? throw new JsonException();
+                        if (categoryModels.Any() && filterRepo is not null)
+                        {
+                            var filters = await filterRepo.GetListBySpec(new FilterSpecs.GetAll());
+                            await categoryRepo.AddRangeAsync(await GetCategories(categoryModels, filters,imageService));
+                            await categoryRepo.SaveAsync();
+                        }
+                    }
+                    catch (JsonException)
+                    {
+                        Console.WriteLine("Error deserialize categories json file");
+                    }
+                }
+                else Console.WriteLine("File \"JsonData/Categories.json\" not found");
+            }
+
+    }
+}
