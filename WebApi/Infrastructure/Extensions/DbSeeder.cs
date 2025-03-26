dbda42ed78c58ce2108c12030cfa24c77572e7c2
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-03-26 20:54:49 +0200

    test

--- WebApi/Infrastructure/Extensions/DbSeeder.cs
+++ WebApi/Infrastructure/Extensions/DbSeeder.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+﻿using Microsoft.AspNetCore.Identity;
+using static System.Formats.Asn1.AsnWriter;
+using Microsoft.Graph.Models;
+using Infrastructure.Entities;
 
 namespace Infrastructure.Extensions
 {
-    public class DbSeeder
+    public static class DbSeeder
     {
+          public static async Task SeedDataAsync(this WebApplication app)
+        {
+
+            using var scope = app.Services.CreateScope();
          //Category seeder
             var categoryRepo = scope.ServiceProvider.GetService<IRepository<Category>>();
             if (categoryRepo is not null && !await categoryRepo.AnyAsync())
@@ -20,7 +28,7 @@ namespace Infrastructure.Extensions
                     var filtersJson = File.ReadAllText(categoryJsonDataFile, Encoding.UTF8);
                     try
                     {
-                        var categoryModels = JsonConvert.DeserializeObject<IEnumerable<SeederCategoryModel>>(filtersJson)
+                        var categoryModels = JsonConvert.DeserializeObject<IEnumerable<CategoryEntity>>(filtersJson)
                             ?? throw new JsonException();
                         if (categoryModels.Any() && filterRepo is not null)
                         {
@@ -36,6 +44,8 @@ namespace Infrastructure.Extensions
                 }
                 else Console.WriteLine("File \"JsonData/Categories.json\" not found");
             }
+        
+          }
 
     }
 }
