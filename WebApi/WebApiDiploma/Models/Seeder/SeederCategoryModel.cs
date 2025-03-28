0a75c91a658d7fbdbf4e65786027de8191aa2af4
sergijolenuk50 <sergijolenuk50@gmail.com>
2025-03-28 20:04:26 +0200

    Add New Category JSON

--- WebApi/WebApiDiploma/Models/Seeder/SeederCategoryModel.cs
+++ WebApi/WebApiDiploma/Models/Seeder/SeederCategoryModel.cs
@@ -1,12 +1,22 @@
 ﻿using Infrastructure.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApiDiploma.Models.Seeder
 {
     public class SeederCategoryModel
     {
         public string Name { get; init; } = string.Empty;
-        public string? Image { get; init; }
-        public IEnumerable<SeederCategoryModel>? Childs { get; init; }
-        public IEnumerable<String>? Filters { get; init; }
+        public string? UrlSlug { get; set; }
+        //public string? Image { get; init; }
+
+        public int Priority { get; set; }
+        public string? Description { get; set; }
+        public long? ParentId { get; set; }
+
+        public IEnumerable<SeederCategoryModel>? Children { get; init; }
+
+        ////public IEnumerable<String>? Filters { get; init; }
+        //public virtual CategoryEntity Parent { get; set; } = null!;
     }
 }
