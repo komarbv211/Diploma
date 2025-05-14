4e7ccb5138d3d2b8b2f8414413d7729cfd634889
komarbv211 <andrijkoslak@gmail.com>
2025-05-14 18:51:05 +0300

    refactor controller, add swager autoriration

--- WebApi/WebApiDiploma/ServiceExtensions/JwtServiceExtensions.cs
+++ WebApi/WebApiDiploma/ServiceExtensions/JwtServiceExtensions.cs
@@ -9,22 +9,7 @@ namespace WebApiDiploma.ServiceExtensions
     public static class JwtServiceExtensions
     {
         public static IServiceCollection AddJwtAuthentication(this IServiceCollection services, JwtOptions jwtOpts)
-        {
-            //services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-            //        .AddJwtBearer(o =>
-            //        {
-            //            o.TokenValidationParameters = new TokenValidationParameters
-            //            {
-            //                ValidateIssuer = true,
-            //                ValidateAudience = false,
-            //                ValidateLifetime = true,
-            //                ValidateIssuerSigningKey = true,
-            //                ValidIssuer = jwtOpts.Issuer,
-            //                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOpts.Key)),
-            //                ClockSkew = TimeSpan.Zero
-            //            };
-            //        });
-
+        {           
             var signinKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOpts.Key));
             services.AddAuthentication(options =>
              {
