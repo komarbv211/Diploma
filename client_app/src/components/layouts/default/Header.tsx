34ce2a1a80f3b16be6f82180566de3a09b134adc
Olia <oliaopanasiukk@gmail.com>
2025-05-18 20:38:04 +0300

    add auth buttons in default layout

--- client_app/src/components/layouts/default/Header.tsx
+++ client_app/src/components/layouts/default/Header.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Dropdown } from 'antd';
+import { Avatar, Button, Dropdown } from 'antd';
 import { UserOutlined, LogoutOutlined, SettingOutlined } from '@ant-design/icons';
 import { Link, useNavigate } from 'react-router-dom';
 import { getUser, logOut } from '../../../store/slices/userSlice'
@@ -19,23 +19,31 @@ const CustomHeader = () => {
     { key: 'profile', icon: <UserOutlined />, label: <Link to={`/profile/${user?.id}`}>Profile</Link> },
     { key: 'logout', icon: <LogoutOutlined />, label: <Link to="/" onClick={handleLogout}>Log Out</Link> },
   ];
-  
   const adminExtraItems = [
-    { key: 'profile', icon: <UserOutlined />, label: <Link to="/admin/profile">Profile</Link> },
     { key: 'settings', icon: <SettingOutlined />, label: <Link to="/admin/settings">Settings</Link> },
-    { key: 'logout', icon: <LogoutOutlined />, label: <Link to="/logout" onClick={handleLogout}>Log Out</Link> },
   ];
 
   // TODO
-  const menuItems = user?.roles.includes('admin') ? [...adminExtraItems, ...commonItems] : commonItems;
+  const menuItems = user?.roles.includes('Admin') ? [...adminExtraItems, ...commonItems] : commonItems;
 
   return (
     <div className="fixed top-0 left-0 right-0 h-16 bg-white shadow-sm z-50">
-      <div className="flex items-center justify-end h-full px-6">
-        <Dropdown menu={{ items: menuItems }}>
-          <Avatar size={42} icon={<UserOutlined />} className="cursor-pointer" />
-        </Dropdown>
-      </div>
+      {user ? (
+        <div className="flex items-center justify-end h-full px-6">
+          <Dropdown menu={{ items: menuItems }}>
+            <Avatar size={42} icon={<UserOutlined />} className="cursor-pointer" />
+          </Dropdown>
+        </div>
+      ) : (
+        <div className="flex justify-end px-6 gap-4">
+          <Link to="/login">
+            <Button>Login</Button>
+          </Link>
+          <Link to="/registr">
+            <Button>Register</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
