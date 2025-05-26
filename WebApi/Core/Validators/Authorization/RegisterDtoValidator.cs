8794ef69ade7332f5fc9eba8b148e979cec6dd17
komarbv211 <andrijkoslak@gmail.com>
2025-05-26 14:25:00 +0300

    add Dto validation

---
+++ WebApi/Core/Validators/Authorization/RegisterDtoValidator.cs
@@ -0,0 +1,36 @@
+﻿using Core.DTOs.AuthorizationDTOs;
+using FluentValidation;
+
+namespace Core.Validators.Authorization
+{
+    public class RegisterDtoValidator : AbstractValidator<RegisterDto>
+    {
+        public RegisterDtoValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotEmpty().WithMessage("First name is required.")
+                .MaximumLength(255)
+                .Matches("^[A-Z]").WithMessage("First name must start with uppercase letter.");
+
+            RuleFor(x => x.LastName)
+                .NotEmpty().WithMessage("Last name is required.")
+                .MaximumLength(255)
+                .Matches("^[A-Z]").WithMessage("Last name must start with uppercase letter.");
+
+            RuleFor(x => x.Email)
+                .NotEmpty().WithMessage("Email is required.")
+                .EmailAddress().WithMessage("Invalid email address.");
+
+            RuleFor(x => x.Password)
+                .NotEmpty().WithMessage("Password is required.")
+                .MinimumLength(6)
+                .Matches("[A-Z]").WithMessage("Password must contain at least one uppercase letter.")
+                .Matches("[a-z]").WithMessage("Password must contain at least one lowercase letter.")
+                .Matches("[0-9]").WithMessage("Password must contain at least one number.");
+
+            RuleFor(x => x.Image)
+                .Must(uri => string.IsNullOrEmpty(uri) || Uri.IsWellFormedUriString(uri, UriKind.Absolute))
+                .WithMessage("Image must be a valid URL.");
+        }
+    }
+}
