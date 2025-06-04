9185563aa64b63add8c1690e66c02a698efd34db
Bohdan <krasovskybogdan2.0@gmail.com>
2025-06-04 20:23:37 +0200

    Add fixes to login with Google

--- client_app/src/services/authApi.ts
+++ client_app/src/services/authApi.ts
@@ -1,55 +1,66 @@
 import { createApi } from '@reduxjs/toolkit/query/react';
-import { IAuthResponse, IGoogleLoginRequest, IGoogleRegister, IUserLoginRequest, IUserRegisterRequest } from '../types/account';
+import {
+    IAuthResponse,
+    IGoogleLoginRequest,
+    IGoogleRegister,
+    IUserLoginRequest,
+    IUserRegisterRequest
+} from '../types/account';
 import { createBaseQuery } from '../utilities/createBaseQuery';
 import { handleAuthQueryStarted } from '../utilities/handleAuthQueryStarted';
 import { serialize } from 'object-to-formdata';
 
 export const authApi = createApi({
-
-  reducerPath: 'accountApi',
-  baseQuery: createBaseQuery('Accounts'),
-  tagTypes: ['AuthUser'],
-  endpoints: (builder) => ({
-    registerUser: builder.mutation<void, IUserRegisterRequest>({
-        query: (userRegister) => ({
-            url: 'register',
-            method: 'POST',
-            body: userRegister,
+    reducerPath: 'accountApi',
+    baseQuery: createBaseQuery('Accounts'),
+    tagTypes: ['AuthUser'],
+    endpoints: (builder) => ({
+        registerUser: builder.mutation<void, IUserRegisterRequest>({
+            query: (userRegister) => ({
+                url: 'register',
+                method: 'POST',
+                body: userRegister,
+            }),
+            invalidatesTags: ['AuthUser'],
         }),
-        invalidatesTags: ['AuthUser'],
-    }),
-    loginUser: builder.mutation<IAuthResponse, IUserLoginRequest>({
-        query: (loginCredentials) => {
-            console.log('Відправка запиту логіну:', loginCredentials);
-            return {
+
+        loginUser: builder.mutation<IAuthResponse, IUserLoginRequest>({
+            query: (loginCredentials) => ({
                 url: 'login',
                 method: 'POST',
                 body: loginCredentials,
-            };
-        },
-        onQueryStarted: handleAuthQueryStarted,
-        invalidatesTags: ['AuthUser'],
-    }),
-    confirmGoogleLogin: builder.mutation<IAuthResponse, IGoogleLoginRequest>({
-        query: (data) => ({
-            url: 'login/google',
-            method: 'POST',
-            body: serialize(data),
+            }),
+            onQueryStarted: handleAuthQueryStarted,
+            invalidatesTags: ['AuthUser'],
         }),
-        onQueryStarted: handleAuthQueryStarted,
-        invalidatesTags: ['AuthUser'],
-    }),
 
-    confirmGoogleRegister: builder.mutation<IAuthResponse, IGoogleRegister>({
-        query: (data) => ({
-            url: 'register/google',
-            method: 'POST',
-            body: serialize(data),
-        }),        
-        onQueryStarted: handleAuthQueryStarted,
-        invalidatesTags: ['AuthUser'],
+        confirmGoogleLogin: builder.mutation<IAuthResponse, IGoogleLoginRequest>({
+            query: (data) => ({
+                url: 'login/google',
+                method: 'POST',
+                body: serialize(data),
+            }),
+            onQueryStarted: handleAuthQueryStarted,
+            invalidatesTags: ['AuthUser'],
+        }),
+
+        confirmGoogleRegister: builder.mutation<IAuthResponse, IGoogleRegister>({
+            query: (data) => ({
+                url: 'register/google',
+                method: 'POST',
+                body: serialize(data),
+            }),
+            onQueryStarted: handleAuthQueryStarted,
+            invalidatesTags: ['AuthUser'],
+        }),
+
+        checkGoogleRegistered: builder.query<{ isGoogleUser: boolean }, string>({
+            query: (email) => ({
+                url: `is-registered-google?email=${encodeURIComponent(email)}`,
+                method: 'GET',
+            }),
+        }),
     }),
-  }),
 });
 
 export const {
@@ -57,4 +68,5 @@ export const {
     useLoginUserMutation,
     useConfirmGoogleLoginMutation,
     useConfirmGoogleRegisterMutation,
+    useLazyCheckGoogleRegisteredQuery,
 } = authApi;
