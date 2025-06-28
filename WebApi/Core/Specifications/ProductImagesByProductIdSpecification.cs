e12fade64a2ec2b9aefc43587059f20454ba975b
komarbv211 <andrijkoslak@gmail.com>
2025-06-28 15:31:37 +0300

    edit HandleProductImagesUpdateAsync

---
+++ WebApi/Core/Specifications/ProductImagesByProductIdSpecification.cs
@@ -0,0 +1,12 @@
+﻿using Ardalis.Specification;
+using Infrastructure.Entities;
+
+public class ProductImagesByProductIdSpecification : Specification<ProductImageEntity>
+{
+    public ProductImagesByProductIdSpecification(long productId)
+    {
+        Query
+            .Where(img => img.ProductId == productId)
+            .OrderBy(img => img.Priority);
+    }
+}
--- client_app/src/routes/adminRoutes.tsx
+++ client_app/src/routes/adminRoutes.tsx
@@ -1,7 +1,6 @@
 import { lazy } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-
 const AdminLayout = lazy(() => import('./../components/layouts/admin/Layout'))
 const Dashboard = lazy(() => import('./../pages/admin/Dashboard'))
 const AdminProfile = lazy(() => import('./../pages/admin/AdminProfile'))
@@ -14,12 +13,12 @@ const CreateProductPage = lazy(() => import('./../pages/admin/products/CreatePro
 const EditProductPage = lazy(() => import('../pages/admin/products/EditProductpage'))
 
 export const AdminRoutes = () => {
-    return (
-        <Routes>
-            <Route element={<AdminLayout />}>
-                <Route index element={<Dashboard />} />
-                <Route path="profile" element={<AdminProfile />} />
-                <Route path="users" element={<UsersPage />} />
+  return (
+    <Routes>
+      <Route element={<AdminLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path="profile" element={<AdminProfile />} />
+        <Route path="users" element={<UsersPage />} />
 
         <Route path="categories">
           <Route index element={<CategoryList />} />
@@ -34,7 +33,6 @@ export const AdminRoutes = () => {
         </Route>
       </Route>
     </Routes>
-
   );
 };
 
