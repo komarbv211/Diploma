43aa4b33af6d5bba074c01ff38817e85fbc4ad38
Olia <oliaopanasiukk@gmail.com>
2025-07-12 22:28:56 +0300

    add create and delete functionality for promotions

--- WebApi/Core/Services/PromotionService.cs
+++ WebApi/Core/Services/PromotionService.cs
@@ -2,20 +2,65 @@
 using Core.DTOs.PromotionDTOs;
 using Core.Interfaces;
 using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Core.Services
 {
     public class PromotionService : IPromotionService
     {
         private readonly IPromotionRepository _promotionRepository;
+        private readonly IRepository<PromotionEntity> _repository;
         private readonly IMapper _mapper;
         private readonly IImageService _imageService;
 
-        public PromotionService(IPromotionRepository promotionRepository, IMapper mapper, IImageService imageService)
+        public PromotionService(
+            IPromotionRepository promotionRepository,
+            IRepository<PromotionEntity> repository,
+            IMapper mapper,
+            IImageService imageService)
         {
             _promotionRepository = promotionRepository;
+            _repository = repository;
             _mapper = mapper;
             _imageService = imageService;
+        }
+
+        public async Task CreatePromotionAsync(PromotionCreateDto dto)
+        {
+            var entity = _mapper.Map<PromotionEntity>(dto);
+
+            entity.StartDate = DateTime.SpecifyKind(dto.StartDate, DateTimeKind.Utc);
+            entity.EndDate = DateTime.SpecifyKind(dto.EndDate, DateTimeKind.Utc);
+
+            if (dto.Image != null)
+            {
+                var imageName = await _imageService.SaveImageAsync(dto.Image);
+                entity.Image = imageName;
+            }
+
+            if (dto.ProductIds != null && dto.ProductIds.Any())
+            {
+                entity.PromotionProducts = dto.ProductIds.Select(id => new PromotionProductEntity
+                {
+                    ProductId = id
+                }).ToList();
+            }
+
+            await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+        }
+
+        public async Task DeletePromotionAsync(long id)
+        {
+            var entity = await _promotionRepository.GetByIdWithProductsAsync(id);
+            if (entity == null)
+                throw new Exception("Promotion not found");
+
+            if (!string.IsNullOrEmpty(entity.Image))
+                _imageService.DeleteImageIfExists(entity.Image);
+
+            await _repository.DeleteAsync(id);
+            await _repository.SaveAsync();
         }
 
         public async Task UpdatePromotionAsync(PromotionUpdateDto dto)
