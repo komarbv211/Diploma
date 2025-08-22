0ef9efdc666e452e87c88a63163a74fc0fd07902
komarbv211 <andrijkoslak@gmail.com>
2025-08-22 12:10:19 +0300

    add product comments

---
+++ WebApi/Core/Interfaces/ICommentService.cs
@@ -0,0 +1,10 @@
+﻿using Core.DTOs.CommentDTOs;
+
+namespace Core.Interfaces;
+
+public interface ICommentService
+{
+    Task<List<CommentItemDto>> GetCommentsByProductIdAsync(long productId);
+    Task<CommentItemDto> AddCommentAsync(CommentCreateDto dto);
+    Task DeleteCommentAsync(long id);
+}
---
+++ WebApi/Core/Validators/Comments/CommentCreateDtoValidator.cs
@@ -0,0 +1,21 @@
+﻿using Core.DTOs.CommentDTOs;
+using FluentValidation;
+
+namespace Core.Validators.Comments
+{
+    public class CommentCreateDtoValidator : AbstractValidator<CommentCreateDto>
+    {
+        public CommentCreateDtoValidator()
+        {
+            RuleFor(c => c.ProductId)
+                .GreaterThan(0).WithMessage("Продукт обов'язковий.");
+
+            RuleFor(c => c.UserId)
+                .GreaterThan(0).WithMessage("Користувач обов'язковий.");
+
+            RuleFor(c => c.Text)
+                .NotEmpty().WithMessage("Текст коментаря не може бути порожнім.")
+                .MaximumLength(1000).WithMessage("Текст коментаря не може перевищувати 1000 символів.");
+        }
+    }
+}
