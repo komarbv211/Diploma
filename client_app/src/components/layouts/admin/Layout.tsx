70f06c5dd8eb520233f58fb4a0d619a833426451
komarbv211 <andrijkoslak@gmail.com>
2025-09-14 10:51:58 +0300

    fix layout

--- client_app/src/components/layouts/admin/Layout.tsx
+++ client_app/src/components/layouts/admin/Layout.tsx
@@ -1,4 +1,4 @@
-import { Breadcrumb, Layout } from "antd";
+import { Layout } from "antd";
 import { Outlet } from "react-router-dom";
 import CustomHeader from "../default/Header";
 import AdminSidebar from "./AdminSidebar";
@@ -18,10 +18,10 @@ const AdminLayout = () => {
         </Sider>
 
         <Content className="m-3 p-6 bg-white overflow-auto mt-7">
-          <Breadcrumb
+          {/* <Breadcrumb
             items={[{ title: "Home" }, { title: "Dashboard" }]}
             className="mb-4"
-          />
+          /> */}
           <Outlet />
         </Content>
       </Layout>
