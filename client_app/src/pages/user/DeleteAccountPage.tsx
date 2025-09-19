b3e0c7b93e49ec2d534aec84aa0ec685c77e3cff
komarbv211 <andrijkoslak@gmail.com>
2025-09-19 19:28:44 +0300

    DeleteAccountPage styling.

--- client_app/src/pages/user/DeleteAccountPage.tsx
+++ client_app/src/pages/user/DeleteAccountPage.tsx
@@ -35,11 +35,14 @@ const DeleteAccountPage = () => {
   return (
     <div className="flex justify-center mt-10">
       <Card className="max-w-lg w-full p-6 text-center shadow-md rounded-xl">
-        <Title level={3} className="text-red-500">Видалити мій акаунт</Title>
+        <Title level={3} >Видалити мій акаунт</Title>
         <Paragraph>
           Ця дія є незворотною. Після видалення акаунта ви втратите доступ до
           всіх даних і замовлень, пов’язаних з вашим профілем.
         </Paragraph>
+        <Button type="default" className="mb-1" onClick={() => navigate(-1)}>
+          Повернутись до профілю
+        </Button>  
         <Popconfirm
           title="Ви впевнені, що хочете видалити акаунт?"
           onConfirm={handleDelete}
@@ -47,12 +50,11 @@ const DeleteAccountPage = () => {
           cancelText="Скасувати"
         >
           <Button
-            danger
             type="primary"
             loading={isLoading}
-            className="mt-4"
+            className="remember-button"
           >
-            Видалити акаунт
+            <span className="remember-button-text gap-[12px]"> Видалити акаунт</span>
           </Button>
         </Popconfirm>
       </Card>
