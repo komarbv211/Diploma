e35b16d200ce2b12424d9602685170636c86da83
komarbv211 <andrijkoslak@gmail.com>
2025-09-24 16:25:34 +0300

    Added carousel for successful order

--- WebApi/Core/Services/BrandService.cs
+++ WebApi/Core/Services/BrandService.cs
@@ -1,19 +1,10 @@
 ﻿using AutoMapper;
 using Core.DTOs.BrandsDTOs;
-using Core.DTOs.CategoryDTOs;
-using Core.DTOs.ProductsDTO;
 using Core.Exceptions;
 using Core.Interfaces;
-using Core.Repositories;
-using Core.Specifications;
 using Infrastructure.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Core.Services
 {
@@ -26,21 +17,8 @@ namespace Core.Services
             _brandRepository = brandRepository;
             _mapper = mapper;
         }
-
-
-
         public async Task<BrandItemDto> CreateBrandAsync(BrandCreateDto dto)
         {
-
-            var brandExists = _brandRepository
-        .GetAllQueryable()
-        .Any(b => b.Name.ToLower() == dto.Name.ToLower());
-
-            if (brandExists)
-            {
-                throw new HttpException($"Бренд з назвою '{dto.Name}' вже існує.", HttpStatusCode.Conflict);
-            }
-
             try
             {
                 var brand = _mapper.Map<BrandEntity>(dto);
@@ -57,53 +35,6 @@ namespace Core.Services
             {
                 throw new HttpException("Невідома помилка при створенні бренду", HttpStatusCode.InternalServerError, ex);
             }
-
-            //try
-            //{
-            //    // Перевірка на існування бренду з такою ж назвою (чутлива до регістру або ні — залежно від потреб)
-            //     var existingBrand = await _brandRepository.FirstOrDefaultAsync(
-            //     b => b.Name.ToLower() == dto.Name.ToLower()
-            //     );
-
-            //    if (existingBrand != null)
-            //    {
-            //        throw new HttpException(
-            //            $"Бренд з назвою '{dto.Name}' вже існує.",
-            //            HttpStatusCode.Conflict
-            //        );
-            //    }
-
-            //    var brand = _mapper.Map<BrandEntity>(dto);
-            //    await _brandRepository.Insert(brand);
-            //    await _brandRepository.SaveAsync();
-
-            //    return _mapper.Map<BrandItemDto>(brand);
-            //}
-            //catch (DbUpdateException dbEx)
-            //{
-            //    throw new HttpException("Помилка при збереженні бренду в базі даних", HttpStatusCode.InternalServerError, dbEx);
-            //}
-            //catch (Exception ex)
-            //{
-            //    throw new HttpException("Невідома помилка при створенні бренду", HttpStatusCode.InternalServerError, ex);
-            //}
-
-            //try
-            //{
-            //    var brand = _mapper.Map<BrandEntity>(dto);
-            //    await _brandRepository.Insert(brand);
-            //    await _brandRepository.SaveAsync();
-
-            //    return _mapper.Map<BrandItemDto>(brand);
-            //}
-            //catch (DbUpdateException dbEx)
-            //{
-            //    throw new HttpException("Помилка при збереженні бренду в базі даних", HttpStatusCode.InternalServerError, dbEx);
-            //}
-            //catch (Exception ex)
-            //{
-            //    throw new HttpException("Невідома помилка при створенні бренду", HttpStatusCode.InternalServerError, ex);
-            //}
         }
 
         public async Task DeleteBrandAsync(long id)
@@ -115,8 +46,6 @@ namespace Core.Services
                 await _brandRepository.SaveAsync();
             }
         }
-
-
 
         public async Task<List<BrandItemDto>> GetBrandsAsync()
         {
---
+++ WebApi/Core/Validators/Brands/BrandCreateValidator.cs
@@ -0,0 +1,28 @@
+﻿using Core.DTOs.BrandsDTOs;
+using Core.Interfaces;
+using FluentValidation;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core.Validators.Brands;
+
+public class BrandCreateValidator : AbstractValidator<BrandCreateDto>
+{
+    private readonly IRepository<BrandEntity> _brandRepository;
+
+    public BrandCreateValidator(IRepository<BrandEntity> brandRepository)
+    {
+        _brandRepository = brandRepository;
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Назва бренду обов'язкова.")
+            .MinimumLength(2).WithMessage("Назва бренду має містити щонайменше 2 символи.")
+            .Matches("^[А-ЯA-Z]").WithMessage("Назва бренду має починатися з великої літери.")
+            .MustAsync(async (name, cancellation) =>
+                !await _brandRepository
+                    .GetAllQueryable()
+                    .AnyAsync(b => b.Name.ToLower() == name.ToLower(), cancellation))
+            .WithMessage("Назва бренду має бути унікальною.");
+    }
+}
